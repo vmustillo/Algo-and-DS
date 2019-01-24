d805e9aa2f946919f57f4d36b4f66d7d77847026
Vincent Mustillo <vjmustillo@gmail.com>
2019-01-23 19:40:17 -0500

    Completed KMP alogirthm

--- KMP.js
+++ KMP.js
@@ -19,8 +19,43 @@ function computeLPSArray(pattern) {
             }
         }
     }
-
-    console.log(lps);
+    console.log("LPS: ", lps);
+    return lps;
 }
 
-computeLPSArray("AABAACAABAA");
+function KMPSearch(pattern, text) 
+{
+    console.log(pattern);
+    console.log(text);
+    var patLength = pattern.length;
+    var textLength = text.length;
+
+    var lps = computeLPSArray(pattern);
+    var textIncrement = 0;
+    var patternIncrement = 0;
+
+    while (textIncrement < textLength) { 
+        console.log("patternIncrement: ", patternIncrement);
+        console.log("textIncrement: ", textIncrement);
+        if (pattern[patternIncrement] === text[textIncrement]) { 
+            patternIncrement++;
+            textIncrement++;
+        }
+        if (patternIncrement === patLength) {
+            console.log("Found pattern at index ", textIncrement - patternIncrement);
+            patternIncrement = lps[patternIncrement - 1]; // Here is the optimization.  It is reverting back to the lps array.  If there is a prefix thatis also a suffix, it can skip to the suffix and check that
+        } else if (textIncrement < textLength && pattern[patternIncrement] !== text[textIncrement]) {  // mismatch after j matches
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if ([patternIncrement] != 0)
+                patternIncrement = lps[patternIncrement - 1];
+            else
+                textIncrement = textIncrement + 1;
+        }
+    }
+}
+
+//computeLPSArray("AABAACAABAA");
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+KMPSearch(pattern, text);
