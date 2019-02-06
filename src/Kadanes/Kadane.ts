a73c582f42a8af57b6138c24b5c754475e00dfe9
Vincent Mustillo <vjmustillo@gmail.com>
2019-02-05 23:55:37 -0500

    Added Kadane's algorithm to the project. Wrote Kadane's algorithm in typescript and described its logic. I also added node_modules, a .gitignore, and a script to run the typescript compiler

---
+++ src/Kadanes/Kadane.ts
@@ -0,0 +1,31 @@
+// Find the maximum sub array given an array of integers
+
+/**
+ * This function returns the sum of the integers of the maximum contiguous subarray in value
+ * The subarray must be contigous meaning all elements of the subarray must be consecutive
+ * The way to calculate this is by using Kadane's algorithm
+ * 
+ * @param {number[] nums}
+ * @return {number}
+ */
+function findMaxSubArray(nums: number[]) {
+    var length: number = nums.length; // precompute length of array so we do not have to more than once
+    if(length === 0) {
+        return 0;
+    } else if(length === 1) {
+        return nums[0];
+    }
+    var globalMax: number = nums[0], currentMax: number = nums[0]; // set global and current maximum subarrays to the first value of the array
+    for(let i: number = 1; i < length; i++) { //currentMax is already the first element, so we can start at i = 1
+        currentMax = Math.max(nums[i], currentMax + nums[i]); // find max between currentMax and currentMax + current element
+        if(currentMax > globalMax) { // If this is greater than the maximum subarray, then this value is the new maximum subarray
+            globalMax = currentMax;
+        }
+    }
+    return globalMax; 
+}
+
+var nums: number[] = [1, 6, -4, 2, 5, 3, -4, -1, 3];
+console.log(findMaxSubArray(nums));
+// Maximum subarray = 13
+// 1 + 6 + (-4) + 2 + 5 + 3
