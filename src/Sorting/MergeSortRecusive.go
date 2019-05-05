ee8e225c4edad7cbb8ac7235e6d4e7cd1891a79d
Vincent Mustillo <vjmustillo@gmail.com>
2019-05-05 12:05:47 -0400

    Finished mergesort

--- src/Sorting/MergeSortRecusive.go
+++ src/Sorting/MergeSortRecusive.go
@@ -2,96 +2,48 @@ package main
 
 import "fmt"
 
-// MergeSort will be used to half the array and 
-// func mergeSort(arr []int) {
-// 	mergeHelper(arr, 0, len(arr) - 1)
-// }
-
-// //
-// func mergeHelper(arr []int, left int, right int) {
-// 	if left < right {
-// 		middle := (left + right) / 2
-// 		mergeHelper(arr, left, middle)
-// 		mergeHelper(arr, middle + 1, right)
-// 		mergeHalves(arr, left, right)
-// 	}
-// }
-
-// // Merge is
-// func mergeHalves(arr []int, left int, right int) {
-// 	leftEnd := (left + right) / 2
-// 	rightStart := leftEnd + 1
-// 	temp := arr
-// 	i := left
-
-// 	for left <= leftEnd && rightStart <= right {
-// 		fmt.Println(temp, left, rightStart)
-// 		if arr[left] <= arr[rightStart] {
-// 			temp[i] = arr[left]
-// 			left++
-// 		} else {
-// 			temp[i] = arr[rightStart]
-// 			rightStart++
-// 		}
-// 		i++
-// 	}
-
-// 	for left <= leftEnd {
-// 		temp[i] = arr[left]
-// 		left++
-// 		i++
-// 	}
-
-// 	for rightStart <= right {
-// 		temp[i] = arr[rightStart]
-// 		rightStart++
-// 		i++
-// 	}
-	
-// 	arr = temp
-// }
-
+// mergeSort first divides the array down into arrays with single values
 func mergeSort(arr []int) []int {
-
-	if(len(arr) == 1) {
+	if len(arr) <= 1 {
 		return arr
 	}
+
 	middle := len(arr) / 2
 
 	return merge(mergeSort(arr[0:middle]), mergeSort(arr[middle:]))
 }
 
+// merge takes two arrays as input and sorts them into a new array.
+// Runs through both arrays comparingelements and inserting the smaller into the new array
+// Once one array has been iterated through, if there is an element left in the other array, we know it is the largest element and it can be appended to the end of the array 
 func merge(leftHalf []int, rightHalf []int) []int {
 	left := 0
 	right := 0
-	leftEnd := len(leftHalf)
-	rightEnd := len(rightHalf)
-	fmt.Println("leftEnd: ", leftEnd)
-	fmt.Println("leftEnd: ", rightEnd)
-	mergedArr := make([]int, leftEnd + rightEnd - 1)
-	i := 0
+	var mergedArr []int
 
-	for left < leftEnd && right < rightEnd {
-		fmt.Println(mergedArr)
+	for left < len(leftHalf) && right < len(rightHalf) {
 		if leftHalf[left] <= rightHalf[right] {
-			mergedArr[i] = leftHalf[left]
+			mergedArr = append(mergedArr, leftHalf[left])
 			left++
 		} else {
-			mergedArr[i] = rightHalf[right]
+			mergedArr = append(mergedArr, rightHalf[right])
 			right++
 		}
-		i++
+	}
+
+	if left < len(leftHalf) {
+		mergedArr = append(mergedArr, leftHalf[left:]...)
+	}
+
+	if right < len(rightHalf) {
+		mergedArr = append(mergedArr, rightHalf[right:]...)
 	}
 	
-	mergedArr = append(mergedArr, leftHalf[left:]...)
-	mergedArr = append(mergedArr, rightHalf[right:]...)
-
 	return mergedArr
 }
 
 func main() {
 	arr := []int{51, 4, 8, 29, 54, 12, 73, 43}
-	//arr := []int{51, 4, 8, 29}
 	fmt.Println("Unsorted array: ", arr)
 	newArr := mergeSort(arr)
 	fmt.Println("Sorted array: ", newArr)
