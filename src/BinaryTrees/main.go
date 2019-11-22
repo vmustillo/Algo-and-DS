7a2cd19e19ef559d32f8f2c5b3d8bd8339c0ea64
Vincent Mustillo <vjmustillo@gmail.com>
2019-11-22 00:42:30 -0500

    Added function to tell if a tree is a subtree of another tree

--- src/BinaryTrees/main.go
+++ src/BinaryTrees/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"bst/tree"
+	"fmt"
 )
 
 func main() {
@@ -21,27 +21,45 @@ func main() {
 	root.Insert(29)
 	root.Insert(42)
 
+	subtreeRoot := tree.NewNode(35)
+	subtreeRoot.Insert(29)
+	subtreeRoot.Insert(42)
+
 	/*
-						25
-					/		\
-				15		     28
-			  /	  \			   \
-			 6	  23			 35
-		   /	 /	\			/  \
-		  4		17	24		  29	42
-			   /  \
-			16     19
+						   SUBTREE
+
+						   35
+						/		\
+					29			42
+
+		/*                 TREE
+
+							25
+						/		\
+					15		     28
+				  /	  \			   \
+				 6	  23			 35
+			   /	 /	\			/  \
+			  4		17	24		  29	42
+				   /  \
+				16     19
 	*/
 
-	fmt.Println("Level-order traversal ")
-	tree.BFTraversal(root)
-	fmt.Println("\nPreorder traversal (Iteratively then Recursively): ")
-	tree.Preorder(root)
-	tree.PreorderRecursive(root)
-	fmt.Println("\nInorder traversal (Iteratively then Recursively): ")
-	tree.Inorder(root)
-	tree.InorderRecursive(root)
-	fmt.Println("\nPostorder traversal (Iteratively then Recursively): ")
-	tree.Postorder(root)
-	tree.PostorderRecursive(root)
+	// fmt.Println("Level-order traversal ")
+	// tree.BFTraversal(root)
+	// fmt.Println("\nPreorder traversal (Iteratively then Recursively): ")
+	// tree.Preorder(root)
+	// tree.PreorderRecursive(root)
+	// fmt.Println("\nInorder traversal (Iteratively then Recursively): ")
+	// tree.Inorder(root)
+	// tree.InorderRecursive(root)
+	// fmt.Println("\nPostorder traversal (Iteratively then Recursively): ")
+	// tree.Postorder(root)
+	// tree.PostorderRecursive(root)
+
+	if tree.IsSubtree(root, subtreeRoot) == true {
+		fmt.Println("true!")
+	} else {
+		fmt.Println("false!")
+	}
 }
